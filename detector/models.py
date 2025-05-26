0aaa920c40b1ab8002b87310f0d3ec39075c1c8a
Ediga Swetha <swethaediga7@gmail.com>
2025-05-26 19:19:22 +0530

    Delete detector directory

--- detector/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class News(models.Model):
-    text = models.TextField()
-    is_fake = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.text[:50]
